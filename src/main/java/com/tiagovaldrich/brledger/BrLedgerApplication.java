46f04ebbaf79c0122e13ad92e272696d168a8e04
Tiago Valdrich <tiago.valdrich@hotmail.com>
2023-05-06 22:02:14 -0300

    chore: rename spring entry point

--- src/main/java/com/tiagovaldrich/brledger/BrLedgerApplication.java
+++ src/main/java/com/tiagovaldrich/brledger/BrLedgerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OpenLedgerApplication {
+public class BrLedgerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OpenLedgerApplication.class, args);
+        SpringApplication.run(BrLedgerApplication.class, args);
     }
 
 }
